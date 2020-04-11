9732ade86971c65162f1bdf302334d4c8deebd97
Lola Garden <lola.c.garden@gmail.com>
2020-04-11 18:26:41 +1200

    Creating the backend with Nest.js

--- server/api-routes/index.tsx
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// import configureApi from "./api";
-
-const configureApi = require('./api');
-const express = require('express');
-const router = express.Router();
-router.use('/api', configureApi); 
-
-export = router;
