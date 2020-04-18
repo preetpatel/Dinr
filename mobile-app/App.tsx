9eda1153ad1a83e03fa84349899539fe3f873c69
gargi-bansal <42624632+gargi-bansal@users.noreply.github.com>
2020-04-18 16:02:29 +1200

    Added splash screen for iOS and Android (#17)
    * Implement splash screen UI, no radial gradient background
    
    * Add iOS splash screen, removed white flash
    
    * Add splash screen for android

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HomeScreen } from "@screens/HomeScreen";
+import SplashScreen from "react-native-splash-screen";
 
 export default function App() {
+  useEffect(() => { SplashScreen.hide(); }, []);
   return <HomeScreen />;
 }
