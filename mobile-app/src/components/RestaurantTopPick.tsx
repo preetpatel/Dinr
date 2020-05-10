a3519cf87c682be7e89920381446b1691ee21d53
Lola <lola.c.garden@gmail.com>
2020-05-10 18:55:09 +1200

    Update restaurantTopPick

--- mobile-app/src/components/RestaurantTopPick.tsx
+++ mobile-app/src/components/RestaurantTopPick.tsx
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, Image, View, TouchableOpacity} from "react-native";
 
 export const RestaurantTopPick: React.FC = () => {
+    const restaurantData = { name: "Paradise", price: 3, disatance: 1.2, image: "../images/salad-plates.png"}
+    const [restaurant, setRestaurant] = useState(restaurantData);
 
     const  viewMorePress = () => {
         // TODO: Add change screen functionality here
     }
 
+    let priceRows = [];
+    for (let i=0; i < restaurant.price; i++) {
+        priceRows.push(<Image key={i} style={styles.priceIcon} source={require("../images/ic_monetization_on_24px.png")}></Image>)
+    }
     return (
-        <View style={styles.restaurantContainer}>
-            <View style={styles.restaurantInfoContainer}>
+        <View style={styles.mainContainer}>
+            <View style={styles.restaurantContainer}>
                 <Image style={styles.restaurantImage} source={require("../images/salad-plates.png")}></Image>
-                <View style={styles.restaurantTextContainer}>
-                    <Text style={styles.restaurantNameText}>Name of place</Text>
-                     <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
+                <View style={styles.restaurantInfoContainer}>
+                    <Text style={styles.nameText}> {restaurant.name} </Text>
+                     <View style={styles.locationInfoContainer}>
                          <Image style={styles.locationIcon} source={require("../images/location-icon.png")}></Image>
-                         <Text style={styles.restaurantDistanceText}>1.2km away</Text>
+                         <Text style={styles.distanceText}> {restaurant.disatance} km away</Text>
                      </View>
                      <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
-                         <Image style={styles.priceIcon} source={require("../images/ic_monetization_on_24px.png")}></Image>
-                         <Image source={require("../images/ic_monetization_on_24px.png")}></Image>
+                       {priceRows}
                      </View>
                 </View>
                 <TouchableOpacity onPress={viewMorePress}>
                     <Image style={styles.viewMoreButton} source={require("../images/green-forward-arrow.png")}></Image>
                 </TouchableOpacity>
             </View>
-            <View style={styles.line}></View>
+            <View style={styles.lineSeparator}></View>
         </View>
 
     );
@@ -34,13 +39,13 @@ export const RestaurantTopPick: React.FC = () => {
 
 
 const styles = StyleSheet.create({
-    restaurantContainer: {
+    mainContainer: {
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
         backgroundColor: "#EEEEEE",
     },
-    restaurantInfoContainer: {
+    restaurantContainer: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
@@ -52,29 +57,34 @@ const styles = StyleSheet.create({
         height: 57,
         width: 57,
         borderColor: "#29732D",
-        borderStyle: "solid",
         borderWidth: 1,
         borderRadius: 5
     },
-    restaurantTextContainer: {
+    restaurantInfoContainer: {
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
         backgroundColor: "#EEEEEE",
-        height: 60,
+        height: 50,
         width: 230
     },
-    restaurantNameText: {
+    nameText: {
         fontFamily: "SFProDisplay-Bold",
         fontWeight: "bold",
         fontSize: 15,
         color: "#29732D",
         lineHeight: 25
     },
+    locationInfoContainer: {
+        display: "flex", 
+        flexDirection: "row", 
+        justifyContent: "flex-start", 
+        marginBottom: 4
+    },
     locationIcon: {
         marginRight: 5
     },
-    restaurantDistanceText: {
+    distanceText: {
         fontFamily: "SFProDisplay-Bold",
         fontSize: 12,
         color: "#29732D",
@@ -85,7 +95,7 @@ const styles = StyleSheet.create({
     viewMoreButton: {
         alignSelf: "flex-end"
     },
-    line: {
+    lineSeparator: {
         backgroundColor: '#E1E1E1',
         height: 1,
         width: "100%"
