a138ffe2b67502567bf1361773c56ab89514e3d2
Lola <lola.c.garden@gmail.com>
2020-05-23 17:34:22 +1200

    Fix failing test

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -5,6 +5,7 @@ import { DistanceService } from './distance/distance.service';
 import { RestaurantsService } from './restaurants/restaurants.service';
 import { FilterService } from './filter/filter.service';
 import { SearchService } from './search/search.service';
+import { SetupService } from './setup/setup.service';
 
 
 describe('AppController', () => {
@@ -14,14 +15,16 @@ describe('AppController', () => {
   let restaurantsService: RestaurantsService;
   let filterService: FilterService;
   let searchService: SearchService;
+  let setupService: SetupService;
 
   beforeEach(async () => {
     distanceService = new DistanceService;
     restaurantsService = new RestaurantsService;
     filterService = new FilterService;
     searchService = new SearchService(restaurantsService, filterService);
+    setupService = new SetupService();
 
-    appService = new AppService(distanceService, searchService);
+    appService = new AppService(distanceService, searchService, setupService);
     appController = new AppController(appService);
   });
 
