b1c8bbb8a0d23c6b85408e7f933ade720c0f5167
Rupert Moran-Adcock <rupert.moran.adcock@gmail.com>
2020-05-23 13:32:58 +1200

    Change backend to get 12 closest restaurants

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,64 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { DistanceService } from './distance/distance.service';
 import { Restaurant } from './models/restaurant';
 import { SearchService } from './search/search.service';
-import { FilterService } from './filter/filter.service';
 
 @Injectable()
 export class AppService {
 
   constructor(
     private distanceService: DistanceService,
-    private filterService: FilterService,
     private searchService: SearchService
     ) {}
 
   originalLat: number;
   originalLon: number;
   allRestaurants: Restaurant[];
-  filteredRestaurants: Restaurant[];
 
   async getRestaurants(lat: number, lon: number, distanceMod: number, 
     cuisines: string, priceRange: number ): Promise<Restaurant[]> {
       this.originalLat = lat;
       this.originalLon = lon;
 
-
       // Search given distance modifier  
-      this.allRestaurants = await this.searchService.expandingSquaresearch(lat, lon, distanceMod); 
-
-      // Filter restaurants
-      this.filteredRestaurants = this.filterService.filterResults(this.allRestaurants, cuisines, priceRange);
+      this.allRestaurants = await this.searchService.expandingSquaresearch(lat, lon, distanceMod, cuisines, priceRange); 
 
       // Get distances for each restaurant that we've gotten back
-      this.filteredRestaurants = await this.getDistancesForRestaurants(this.filteredRestaurants, distanceMod);
+      this.allRestaurants = await this.getDistancesForRestaurants(this.allRestaurants, distanceMod);
 
-      console.log(this.filteredRestaurants);
-      return this.filteredRestaurants;
+      return this.allRestaurants;
   }
 
   async getDistancesForRestaurants(restaurants: Restaurant[], distanceMod: number): Promise<Restaurant[]> {
-
-    let distance: number = 0;
-
-    let index: number;
-
     for (let restaurant of restaurants){
-      distance = await this.distanceService.getDistance(this.originalLat, this.originalLon, restaurant.latitude, restaurant.longitude);
-      if(distance <= distanceMod){
-        restaurant.distance = distance;
-        // index = restaurants.indexOf(restaurant);
-        // restaurants.splice(index,1);
-      }
+      restaurant.distance = await this.distanceService.getDistance(this.originalLat, this.originalLon, restaurant.latitude, restaurant.longitude);
     } 
-
-    // Filter out any restaurants that are too far away
-    for(let restaurant of restaurants){
-      if(restaurant.distance == null){
-        index = restaurants.indexOf(restaurant);
-        restaurants.splice(index, 1);
-      }
-    }
-
     return restaurants;
   }
 }
--- server/src/filter/filter.service.ts
+++ server/src/filter/filter.service.ts
@@ -4,35 +4,29 @@ import { Restaurant } from 'src/models/restaurant';
 @Injectable()
 export class FilterService {
 
-    public filterResults(allRestaurants: Restaurant[], cuisines: string, priceRange: number): Restaurant[] {
-        let filteredRestaurants: Restaurant[] = this.filterByCuisines(allRestaurants, cuisines);
-        filteredRestaurants = this.filterByPriceRange(filteredRestaurants, priceRange);
-        return filteredRestaurants;
+    public checkRestaurant(restaurant: Restaurant, cuisines: string, priceRange: number): boolean{
+        if(this.checkCuisine(restaurant, cuisines) && this.checkPrice(restaurant, priceRange)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private filterByCuisines(allRestaurants: Restaurant[], cuisines: string): Restaurant[] {
+    private checkCuisine(restaurant: Restaurant, cuisines: string): boolean{
         let cuisineList: string[] = cuisines.toLowerCase().split(" ");
-        let filteredRestaurants: Restaurant[] = [];
-
-        allRestaurants.forEach(restaurant => {
-            for (let cuisine of cuisineList) {
-                if (restaurant.cuisines.toLowerCase().includes(cuisine)) {
-                    filteredRestaurants.push(restaurant);
-                }
-            }
-        });
-
-        return filteredRestaurants;
-    }
-
-    private filterByPriceRange(allRestaurants: Restaurant[], priceRange: number): Restaurant[] {
-        let filteredRestaurants: Restaurant[] = [];
-
-        for(let restaurant of allRestaurants){
-            if(restaurant.priceRange <= priceRange){
-                filteredRestaurants.push(restaurant);
+        for (let cuisine of cuisineList) {
+            if (restaurant.cuisines.toLowerCase().includes(cuisine)) {
+                return true;
             }
         }
-        return filteredRestaurants;
+        return false;
+    }
+
+    private checkPrice(restaurant: Restaurant, priceRange: number): boolean{
+        if(restaurant.priceRange <= priceRange){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
